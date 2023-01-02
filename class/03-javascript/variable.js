fde24bd8d7f84514c17137ea0e437c400b0b0753
DevLJW <jaewookv123@gmail.com>
2023-01-02 21:55:10 +0900

    right page div add

---
+++ class/03-javascript/variable.js
@@ -0,0 +1,12 @@
+let classmate = {
+	name: "철수",
+	age: 8,
+  school: "꽃샘초등학교"
+}
+
+// 객체의 값 꺼내기
+console.log(classmate.name)        // 철수
+classmate['name']     // 철수
+console.log(classmate.name)
+// 객체의 키&값 삭제하기
+
