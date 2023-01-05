9ff74816c4aa3cf487c228a729c93eaf606728ff
DevLJW <jaewookv123@gmail.com>
2023-01-05 23:40:29 +0900

    epilogue connecting code add

---
+++ cyworld/game.js
@@ -0,0 +1,25 @@
+
+
+const startWord = () => {
+
+    let myWord = document.getElementById("myword").value //내가 입력한글자
+    let word  =  document.getElementById("word").innerText //제시어 글자
+
+    let lastword = word[word.length-1] //마지막 글자
+    let firstword = myWord[0]          // 첫번쨰 글자
+   
+    if(lastword === firstword){
+    document.getElementById("result").innerText = "정답 입니다."
+    document.getElementById("word").innerText = myWord
+    document.getElementById("myword").value = ""
+    }
+    
+    else{
+
+        document.getElementById("result").innerText = "땡"
+        document.getElementById("myword").innerText = ""
+
+    }
+
+
+}
